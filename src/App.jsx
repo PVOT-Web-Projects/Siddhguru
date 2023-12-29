1bce069c893f2421b46830e9d519d4d953dfbe70
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-12-29 17:04:28 +0530

    final fix v1.4

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/siddhguru" element={<Home />} />
+        <Route path="/Siddhguru" element={<Home />} />
         <Route />
       </Routes>
     </Layout>
